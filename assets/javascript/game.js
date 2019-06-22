2a6dc4bd345a6fcde5882ab5af83665d6b8f7ef9
Miles Standoffish <toddnan@gmail.com>
2019-06-22 13:31:08 -0500

    styling mostly done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,45 +2,91 @@
 
 //hello world!
 
-//variables for use throughout functions
-var avengers = ['captain america', 'iron man', 'thor', 'hulk', 'captain marvel', 'thanos'];
-var selectedAvenger = "";
-var lettersInAvenger = [];
-var numOfBlanks = 0;
-var guessesPerGame = 12;
-var answer = [];
-var guessedLetters = [];
+if ($(document).ready) {
 
-//screen elements
-$winCounter = $('#win-counter');
-$lossCounter = $('#loss-counter');
-$guessesLeft = $('#guesses-left');
-$wrongGuesses = $("#wrong-guesses");
-$wordBlanks = $('#word-blanks');
+    //variables for use throughout functions
+    var avengers = ['captain america', 'iron man', 'thor', 'hulk', 'captain marvel', 'thanos'];
+    var selectedAvenger = "";
+    var lettersInAvenger = [];
+    var numOfBlanks = 0;
+    var guessesPerGame = 12;
+    var answer = []; 
+    var wrongGuesses = [];
+    var gameStarted = false;
 
-
-function startGame() {
-    // reset counters
-    $guessesLeft = guessesPerGame;
-
-    //pick a word for the puzzle
+    //screen elements
+    $communications = $('#game-master');
+    $winCounter = $('#win-counter');
+    $lossCounter = $('#loss-counter');
+    $guessesLeft = $('#guesses-left');
+    $wrongGuesses = $("#wrong-guesses");
+    $wordBlanks = $('#word-blanks');
 
 
-    // update the screen with the new word
+    function startGame() {
+        // reset counters
+        $guessesLeft = guessesPerGame;
 
-    //update guessed letters list
+        //pick a word for the puzzle
+        selectedAvenger = friends[Math.floor(Math.random() * friends.length)];
+        lettersInAvenger = selectedAvenger.split("");
+        numOfBlanks = lettersInAvenger.length;
 
+        //update the screen with the new word
+        for(var i=0; i<lettersInAvenger.length; i++){
+            answer.push("_");
+        }
 
+        //update guessed letters list
+        $wrongGuesses = wrongGuesses;
+        $communications.text("Type a letter to guess!")
+        gameStarted = true;
+    }
+
+    function guess(letter) {
+        console.log("player guessed: " + letter);
+        
+        if (lettersInAvenger.includes(letter)) {
+            //right guess
+            console.log("right guess");
+            /*
+            for (var i = 0; i < numOfBlanks; i++){
+                if(lettersInAvenger.includes(letter) && lettersInAvenger[i] == letter){
+                    answer[i] = letter;
+                }
+                else{
+                
+                }
+            }
+            */
+        } 
+        else {
+            //wrong guess
+            guesses--;
+            guessedLetters.push(letter);
+            $(guesses);
+            $("guessed-letters-list").html(gussedLetters);
+        }
+       
+        }
+    }
+
+    function rewardPlayer() {
+        
+
+    }
+
+    // capture keypress
+    $(function() {
+        $(window).keypress(function(e) {
+            var ev = e || window.event;
+            var key = ev.keyCode || ev.which;
+            if (!gameStarted) { //start game
+                startGame();
+            } else { // user has guessed a letter
+                guess(String.fromCharCode(key));
+            }
+        });
+    });
 }
-
-function guess() {
-
-}
-
-function rewardPlayer() {
-
-}
-
-$("window").keypress(function () {
-    console.log("Handler for .keypress() called.");
-});
+    
